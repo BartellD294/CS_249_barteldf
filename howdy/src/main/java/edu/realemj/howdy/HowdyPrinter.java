1cdff537ce9e6dec57ee508572e1b2d1f57478ea
Dr. Michael J. Reale <realemj@sunypoly.edu>
2023-08-28 13:23:16 -0400

    Not absolute garbage

--- howdy/src/main/java/edu/realemj/howdy/HowdyPrinter.java
+++ howdy/src/main/java/edu/realemj/howdy/HowdyPrinter.java
@@ -2,7 +2,8 @@ package edu.realemj.howdy;
 
 public class HowdyPrinter {
     public static void main(String [] args) {
-        System.out.println("HELLO!");
+
+        System.out.println("HELLO!  It's me.");
     }
 
     public static String getPassword() {
