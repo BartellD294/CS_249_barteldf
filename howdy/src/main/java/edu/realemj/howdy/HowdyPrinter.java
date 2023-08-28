1d25981c34eb72064f4274a664455b441b38cee3
Dr. Michael J. Reale <realemj@sunypoly.edu>
2023-08-28 13:33:44 -0400

    More messages

--- howdy/src/main/java/edu/realemj/howdy/HowdyPrinter.java
+++ howdy/src/main/java/edu/realemj/howdy/HowdyPrinter.java
@@ -4,6 +4,7 @@ public class HowdyPrinter {
     public static void main(String [] args) {
 
         System.out.println("HELLO!  It's me.");
+        System.out.println("How are you?");
     }
 
     public static String getPassword() {
