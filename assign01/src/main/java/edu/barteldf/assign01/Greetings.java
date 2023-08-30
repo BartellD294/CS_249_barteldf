26bdd43a33f4031de41444769a56e02f2be1c2ba
Dylan Bartell <barteldf@sunypoly.edu>
2023-08-30 13:17:13 -0400

    Created assignment file

--- assign01/src/main/java/edu/barteldf/assign01/Greetings.java
+++ assign01/src/main/java/edu/barteldf/assign01/Greetings.java
@@ -2,7 +2,7 @@ package edu.barteldf.assign01;
 
 public class Greetings
 {
-    public static void main(String[] argc)
+    public static void main(String[] args)
     {
         System.out.println("Hello World!");
     }
