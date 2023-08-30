76de8c7a6002caa7d983803a2fffe73fc0266ce9
Dr. Michael J. Reale <realemj@sunypoly.edu>
2023-08-30 12:27:59 -0400

    Added more coolness

--- howdy/src/main/java/edu/realemj/howdy/HowdyPrinter.java
+++ howdy/src/main/java/edu/realemj/howdy/HowdyPrinter.java
@@ -2,7 +2,7 @@ package edu.realemj.howdy;
 
 public class HowdyPrinter {
     public static void main(String [] args) {
-
+        System.out.println("*********************");
         System.out.println("HELLO!  It's me.");
         System.out.println("How are you?");
         System.out.println("I'm fine!");
