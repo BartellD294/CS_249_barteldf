964e6aea0b406147eb1439eb67645c5b70d2a5a9
Dr. Michael J. Reale <realemj@sunypoly.edu>
2023-09-06 13:30:11 -0400

    Started on exercise 02

---
+++ exercises/src/main/java/edu/realemj/exercises02/Wasteland.java
@@ -0,0 +1,7 @@
+package edu.realemj.exercises02;
+
+public class Wasteland {
+    public static void main(String [] args) {
+        System.out.println("I don't want to set the world on fire...");
+    }
+}
---
+++ exercises/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module exercisesmod {
+    exports edu.realemj.exercises02;
+}
