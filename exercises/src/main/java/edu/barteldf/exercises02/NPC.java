2e271d20ec54752738a1fe15eeabfde9722c8368
Dylan Bartell <barteldf@sunypoly.edu>
2023-09-11 12:37:24 -0400

    Created NPC constructors and relationship attributes

--- exercises/src/main/java/edu/barteldf/exercises02/NPC.java
+++ exercises/src/main/java/edu/barteldf/exercises02/NPC.java
@@ -3,13 +3,42 @@ package edu.barteldf.exercises02;
 public class NPC {
     private int health = 100;
 
+    private char relationship = 'N';
+
+    public NPC() {}
+
+    public NPC(int h, char r)        //JUST "public <name of class>", nothing else
+    {
+        health = h;
+        relationship = r;
+    }
+
     public int getHealth() {
         return health;
     }
 
-    public void setHealth(int h) {
+    public void setHealth(int h)
+    {
         health = h;
         health = Math.max(health, 0);
         health = Math.min(health, 100);
+    }
+
+    public char getRelationship()
+    {
+        return relationship;
+    }
+
+    public boolean setRelationship(char r)
+    {
+        boolean valid = false;
+        //F = friend, N = neutral, E = enemy
+        r = Character.toUpperCase(r);
+        if ((r=='F')||(r=='N')||(r=='E'))
+        {
+            relationship=r;
+            valid = true;
+        }
+        return valid;
     }
 }
