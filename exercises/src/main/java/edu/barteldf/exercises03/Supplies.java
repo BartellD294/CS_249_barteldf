2e08bfb74c21a92134a146116227a5e87527afe4
Dylan Bartell <barteldf@sunypoly.edu>
2023-09-20 13:53:31 -0400

    Created OregonTrail and GeneralStore

--- exercises/src/main/java/edu/barteldf/exercises03/Supplies.java
+++ exercises/src/main/java/edu/barteldf/exercises03/Supplies.java
@@ -3,11 +3,15 @@ package edu.barteldf.exercises03;
 public class Supplies
 {
     private double totalFood;
+    private int totalBullets;
+    private int totalAxles;
 
     public double getTotalFood()
     {
         return totalFood;
     }
+    public int getTotalBullets() {return totalBullets;}
+    public int getTotalAxles() {return totalAxles;}
 
     public boolean setTotalFood(double food)
     {
@@ -18,5 +22,38 @@ public class Supplies
             valid = true;
         }
         return valid;
+    }
+
+    public boolean setTotalBullets(int bullets)
+    {
+        boolean valid = false;
+        if (bullets >=0)
+        {
+            totalBullets = bullets;
+            valid = true;
+        }
+        return valid;
+    }
+
+    public boolean setTotalAxles(int axles)
+    {
+        boolean valid = false;
+        if (axles >=0)
+        {
+            totalAxles = axles;
+            valid = true;
+        }
+        return valid;
+    }
+
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("***** SUPPLIES *****\n");
+        sb.append("\t* Food: " + totalFood + "\n");
+        sb.append("\t* Bullets: " + totalBullets + "\n");
+        sb.append("\t* Axles: " + totalAxles + "\n");
+        sb.append("********************\n");
+        return sb.toString();
     }
 }
---
+++ exercises/src/main/java/edu/barteldf/exercises06/OregonTrail.java
@@ -0,0 +1,15 @@
+package edu.barteldf.exercises06;
+import edu.barteldf.exercises03.*;
+import java.util.*;
+
+public class OregonTrail
+{
+    public static void main(String [] args)
+    {
+        Scanner input = new Scanner(System.in);
+        Supplies supplies = new Supplies();
+        GeneralStore store = new GeneralStore();
+        store.enterStore(supplies, input);
+        System.out.println(supplies);
+    }
+}
