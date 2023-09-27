3162c90163e13b1770482b6572bac896d78c09e7
Dylan Bartell <barteldf@sunypoly.edu>
2023-09-27 17:37:10 -0400

    Began creating Superhero class

--- assign02/src/main/java/edu/barteldf/assign02/League.java
+++ assign02/src/main/java/edu/barteldf/assign02/League.java
@@ -1,4 +1,9 @@
 package edu.barteldf.assign02;
 
-public class League {
+public class League
+{
+    public static void main (String [] args)
+    {
+
+    }
 }
--- assign02/src/main/java/edu/barteldf/assign02/Superhero.java
+++ assign02/src/main/java/edu/barteldf/assign02/Superhero.java
@@ -1,4 +1,52 @@
 package edu.barteldf.assign02;
 
-public class Superhero {
+public class Superhero
+{
+    private String heroName = "";
+    private String realName = "";
+    private int height = 0;
+    private double weight = 0.0;
+
+    public String getHeroName()
+    {
+        return heroName;
+    }
+    public String getRealName()
+    {
+        return realName;
+    }
+    public int getHeight()
+    {
+        return height;
+    }
+    public double getWeight()
+    {
+        return weight;
+    }
+    public void setHeroName(String name)
+    {
+        heroName = name;
+    }
+    public void setRealName(String name)
+    {
+        realName = name;
+    }
+    public void setHeight(int totalInches)
+    {
+        height = totalInches;
+    }
+    public void setWeight(double pounds)
+    {
+        weight = pounds;
+    }
+    public int calculateBMI()
+    {
+        //TODO
+        return 0;
+    }
+    public String getNameString()
+    {
+        //TODO
+        return "";
+    }
 }
