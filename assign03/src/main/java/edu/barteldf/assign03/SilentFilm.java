0eef8b976208114ab864ba9e628a99acc5d814a8
Dylan Bartell <barteldf@sunypoly.edu>
2023-10-11 14:11:33 -0400

    Completed  SilentFilm stubs

--- assign03/src/main/java/edu/barteldf/assign03/SilentFilm.java
+++ assign03/src/main/java/edu/barteldf/assign03/SilentFilm.java
@@ -5,15 +5,27 @@ public class SilentFilm
 {
     public static Narrator askForNarration(Scanner input)
     {
-        //TODO
-        char boundaryCharacter = ' ';
-        String [] allLines = {""};
+        char boundaryCharacter;
+        int numLines;
+        System.out.println("Enter boundary character:");
+        boundaryCharacter = input.nextLine().charAt(0);
+        System.out.println("Enter number of lines:");
+        numLines = Integer.parseInt(input.nextLine());
+        String [] allLines = new String [4];
+        System.out.println("Enter lines:");
+        for (int i = 0; i< numLines;i++)
+        {
+            allLines[i] = input.nextLine();
+        }
         Narrator newNarrator = new Narrator(allLines, boundaryCharacter);
         return newNarrator;
     }
 
     public static void main(String [] args)
     {
-        //TODO
+        Scanner in = new Scanner(System.in);
+        Narrator n = askForNarration(in);
+        System.out.println("Our story begins...");
+        System.out.println(n);
     }
 }
