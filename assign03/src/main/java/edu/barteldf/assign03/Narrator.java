87abe8de422876cec189850dfa89f2d8a91f120b
Dylan Bartell <barteldf@sunypoly.edu>
2023-10-11 17:32:48 -0400

    Continued work on Narrator

--- assign03/src/main/java/edu/barteldf/assign03/Narrator.java
+++ assign03/src/main/java/edu/barteldf/assign03/Narrator.java
@@ -2,6 +2,9 @@ package edu.barteldf.assign03;
 
 public class Narrator
 {
+    private final int CARD_WIDTH = 50;
+    private final int CARD_HEIGHT = 8;
+    private String [] lines;
     private char boundaryChar;
     public Narrator(String [] lines, char boundaryChar)
     {
@@ -14,30 +17,89 @@ public class Narrator
     }
     public String getLines()
     {
-        //TODO
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (String str:lines)
+        {
+            sb.append(str);
+            sb.append("\n");
+        }
+        return sb.toString();
     }
     public void setBoundaryChar(char boundaryChar)
     {
         this.boundaryChar = boundaryChar;
     }
-    public void setLines(String [] lines)
+    public void setLines(String [] inLines)
     {
-        //TODO
+        this.lines = new String [inLines.length];
+        for (int i = 0; i<this.lines.length;i++)
+        {
+            this.lines[i]=inLines[i];
+        }
     }
     public String generateBoundaryLine()
     {
-        //TODO
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i<CARD_WIDTH;i++)
+        {
+            sb.append(boundaryChar);
+        }
+        sb.append("\n");
+        return sb.toString();
     }
     public String generateCenteredLine(String text)
     {
-        //TODO
-        return "";
+        int numSpacesNeeded, firstHalfSpaces, secondHalfSpaces;
+        StringBuilder sb = new StringBuilder();
+        sb.append(boundaryChar);
+        numSpacesNeeded = CARD_WIDTH - text.length()-2;
+        firstHalfSpaces = numSpacesNeeded/2;
+        secondHalfSpaces = numSpacesNeeded-firstHalfSpaces;
+        for (int i = 0; i<firstHalfSpaces;i++)
+        {
+            sb.append(" ");
+        }
+        sb.append(text);
+        for (int i = 0; i<secondHalfSpaces;i++)
+        {
+            sb.append(" ");
+        }
+        sb.append(boundaryChar);
+        sb.append("\n");
+        return sb.toString();
     }
     public String toString()
     {
-        //TODO
-        return "";
+        int lineCount;
+        int extraLineCount;
+        int linesLeft;
+        StringBuilder sb = new StringBuilder();
+        for (int lineNum = 0; lineNum < lines.length; lineNum += 4)
+        {
+            linesLeft = lines.length-lineNum-1;
+            sb.append(generateBoundaryLine());
+            sb.append(generateCenteredLine(" "));
+            if (linesLeft < 4)
+            {
+                lineCount = linesLeft;
+                extraLineCount = 4-linesLeft;
+            }
+            else
+            {
+                lineCount = 4;
+                extraLineCount = 0;
+            }
+            for (int j = 0; j<lineCount;j++)
+            {
+                sb.append(lines[lineNum+j]);
+            }
+            for (int j = 0; j<extraLineCount;j++)
+            {
+                sb.append(generateCenteredLine(" "));
+            }
+            sb.append(generateCenteredLine(" "));
+            sb.append(generateBoundaryLine());
+        }
+        return sb.toString();
     }
 }
