99e1f16b163a378fd01dcd4f403295d50ea47341
Dylan Bartell <barteldf@sunypoly.edu>
2023-10-11 13:44:02 -0400

    Finished stubbing Narrator and SilentFilm classes

--- assign03/src/main/java/edu/barteldf/assign03/Narrator.java
+++ assign03/src/main/java/edu/barteldf/assign03/Narrator.java
@@ -1,4 +1,43 @@
 package edu.barteldf.assign03;
 
-public class Narrator {
+public class Narrator
+{
+    private char boundaryChar;
+    public Narrator(String [] lines, char boundaryChar)
+    {
+        setBoundaryChar(boundaryChar);
+        setLines(lines);
+    }
+    public char getBoundaryChar()
+    {
+        return boundaryChar;
+    }
+    public String getLines()
+    {
+        //TODO
+        return "";
+    }
+    public void setBoundaryChar(char boundaryChar)
+    {
+        this.boundaryChar = boundaryChar;
+    }
+    public void setLines(String [] lines)
+    {
+        //TODO
+    }
+    public String generateBoundaryLine()
+    {
+        //TODO
+        return "";
+    }
+    public String generateCenteredLine(String text)
+    {
+        //TODO
+        return "";
+    }
+    public String toString()
+    {
+        //TODO
+        return "";
+    }
 }
--- assign03/src/main/java/edu/barteldf/assign03/SilentFilm.java
+++ assign03/src/main/java/edu/barteldf/assign03/SilentFilm.java
@@ -1,4 +1,19 @@
 package edu.barteldf.assign03;
+import java.util.Scanner;
 
-public class SilentFilm {
+public class SilentFilm
+{
+    public static Narrator askForNarration(Scanner input)
+    {
+        //TODO
+        char boundaryCharacter = ' ';
+        String [] allLines = {""};
+        Narrator newNarrator = new Narrator(allLines, boundaryCharacter);
+        return newNarrator;
+    }
+
+    public static void main(String [] args)
+    {
+        //TODO
+    }
 }
