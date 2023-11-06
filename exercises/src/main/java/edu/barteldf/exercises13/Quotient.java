dfc4561e51a3fd59aaad5f3c28a2503381e55ba1
Dylan Bartell <barteldf@sunypoly.edu>
2023-11-06 13:05:39 -0500

    Fixed UML diagram for assign04

--- exercises/src/main/java/edu/barteldf/exercises13/Quotient.java
+++ exercises/src/main/java/edu/barteldf/exercises13/Quotient.java
@@ -1,22 +1,44 @@
 package edu.barteldf.exercises13;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+
+import java.util.*;
+import java.io.*;
 
 public class Quotient
 {
+
+    public static void badFileStuff() throws IOException
+    {
+        throw new IOException("NOPE!");
+    }
+
+    public static void badArgs()
+    {
+        throw new IllegalArgumentException("BAD ARGS!");
+    }
     public static int quotient(int number1, int number2)
     {
+        if(number2 == 0)
+        {
+            throw new ArithmeticException("YOU FOOL! YOU SHALL TEAR THE UNIVERSE ASUNDER!");
+        }
+
         return number1/number2;
     }
+
     public static void main(String [] args)
     {
         Scanner input = new Scanner(System.in);
-        try {
+
+        try
+        {
             System.out.println("Enter two numbers:");
             int x = input.nextInt();
             int y = input.nextInt();
             int z = quotient(x, y);
             System.out.println("Answer is: " + z);
+
+            badArgs();
+            badFileStuff();
         }
         catch(ArithmeticException e)
         {
@@ -25,13 +47,20 @@ public class Quotient
         }
         catch(InputMismatchException e)
         {
-            System.err.println("");
-            System.err.println(e.getMessage());
+            System.err.println("THAT'S NOT A NUMBER!!!!");
         }
+        catch(IOException e)
+        {
+            System.err.println("Huh...that's odd.");
+        }
+        catch(Exception e)
+        {
+            System.err.println("SOMETHING UNSPECIFIC HAPPENED.");
+            System.out.println(e.getMessage());
+            System.err.println(e);
+            e.printStackTrace();
+        }
+
         System.out.println("DONE!");
-    }
-    private String getMessage()
-    {
-        return "fndsufnsdnk,error!!!!!";
     }
 }
