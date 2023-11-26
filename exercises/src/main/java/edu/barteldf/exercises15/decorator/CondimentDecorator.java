583589398d251cbf2436dcfcea22f21286a4c1f6
Dylan Bartell <barteldf@sunypoly.edu>
2023-11-26 17:04:17 -0500

    more exercises15

--- exercises/src/main/java/edu/barteldf/exercises15/decorator/CondimentDecorator.java
+++ exercises/src/main/java/edu/barteldf/exercises15/decorator/CondimentDecorator.java
@@ -2,4 +2,5 @@ package edu.barteldf.exercises15.decorator;
 
 public abstract class CondimentDecorator extends Beverage {
     public abstract String getDescription();
+    public abstract double getCost();
 }
